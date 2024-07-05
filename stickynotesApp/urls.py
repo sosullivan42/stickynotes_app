ac4c3b72dc7463351a30d12de26e0dfabb63a20a
Sorcha O'Sullivan <152802255+sosullivan42@users.noreply.github.com>
2024-07-05 23:14:00 +0100

    Update urls.py

--- stickynotesApp/urls.py
+++ stickynotesApp/urls.py
@@ -1,23 +1,13 @@
-"""
-URL configuration for stickynotes project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# stickynotesApp/urls.py
 from django.contrib import admin
 from django.urls import include, path
 
+# Define URL patterns for the entire project.
 urlpatterns = [
+    # Admin URL pattern, mapping to the Django admin interface
     path('admin/', admin.site.urls),
+
+    # Include URL patterns from the 'stickynotes' app
+    # All URLs from 'stickynotes.urls' will be prefixed with stickynotes/'
     path('', include('stickynotes.urls'))
 ]
