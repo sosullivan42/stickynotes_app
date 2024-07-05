bfd83aa36ba088f9729370feca20a2b9a02bb739
Sorcha O'Sullivan <152802255+sosullivan42@users.noreply.github.com>
2024-07-05 23:11:27 +0100

    Update models.py

--- stickynotes/models.py
+++ stickynotes/models.py
@@ -1,9 +1,20 @@
+# stickynotes/models.py
 from django.db import models
 
-# Create your models here.
-class StickyNote(models.Model): 
+"""
+Model representing a stickynotes application.
+
+Fields:
+ - title: Charfield for the stickynote title with a maximum length of 50
+ characters.
+ - description: TextField for the stickynote description.
+ - mark_as_complete: BooleanFiels set initially to False and True when
+ complete.
+ - created_by: CharField for the stickynote creator to be named (max
+ length 50 characters).
+"""
+class StickyNote(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     mark_as_complete = models.BooleanField(default=False)
     created_by = models.CharField(max_length=50)
-
