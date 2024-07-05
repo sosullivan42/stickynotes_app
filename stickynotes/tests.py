cffed122c31f78a51a3d77da225227c9b8f26bfb
Sorcha O'Sullivan <152802255+sosullivan42@users.noreply.github.com>
2024-07-05 23:12:26 +0100

    Update tests.py

--- stickynotes/tests.py
+++ stickynotes/tests.py
@@ -1,58 +1,80 @@
+# stickynotes/tests.py
 from django.test import TestCase
 from django.urls import reverse
 from .models import StickyNote
 
-# # Testing of the models.
+# Testing of the models.
 
 class StickyNoteModelTest(TestCase): 
     def setUp(self): 
-        StickyNote.objects.create(title='Test StickyNote', description = 'This is a test stickynote.', mark_as_complete = False, created_by='Test Creator')
+        # Creates a StickyNote object for testing.
+        StickyNote.objects.create(
+                                title='Test StickyNote',
+                                description = 'This is a test stickynote.',
+                                mark_as_complete = False,
+                                created_by='Test Creator')
 
-    def test_stickynote_has_title(self): 
+    def test_stickynote_has_title(self):
+        # Test that the StickyNote object has the expected title.
         stickynote = StickyNote.objects.get(id=1)
         self.assertEqual(stickynote.title, 'Test StickyNote')
 
-    def test_stickynote_has_description(self): 
+    def test_stickynote_has_description(self):
+        # Test that the StickyNote object has the expected description.
         stickynote = StickyNote.objects.get(id=1)
         self.assertEqual(stickynote.description, 'This is a test stickynote.')
 
-    def test_stickynote_is_initally_marked_incomplete(self): 
+    def test_stickynote_is_initally_marked_incomplete(self):
+        # Test that the StickyNote object has mark_as_complete set as False.
         stickynote = StickyNote.objects.get(id=1)
         self.assertEqual(stickynote.mark_as_complete, False)
 
-    def test_stickynote_has_creator(self): 
+    def test_stickynote_has_creator(self):
+        # Test that the StickyNote object the expected creator.
         stickynote = StickyNote.objects.get(id=1)
         self.assertEqual(stickynote.created_by, 'Test Creator')
 
 # Testing of the application view.
-class StickyNoteViewTest(TestCase): 
+
+class StickyNoteViewTest(TestCase):
     @classmethod
-    def setUpTestData(cls): 
+    def setUpTestData(cls):
+        # Create StickyNote objects for testing.
         StickyNote.objects.create(
-        title='Test StickyNote', description = 'This is a test stickynote.', mark_as_complete = False, created_by='Test Creator')
+                                title='Test StickyNote',
+                                description = 'This is a test stickynote.',
+                                mark_as_complete = False,
+                                created_by='Test Creator')
 
         StickyNote.objects.create(
-        title='Test StickyNote', description = 'This is a test stickynote.', mark_as_complete = False, created_by='Test Creator')
+                                title='Test StickyNote',
+                                description = 'This is a test stickynote.',
+                                mark_as_complete = False,
+                                created_by='Test Creator')
 
-
-    def test_stickynote_get_all_returns_all_stickynotes(self): 
+    def test_stickynote_get_all_returns_all_stickynotes(self):
+        # Test that 'get_all' displays all existing stickynotes.
         response = self.client.get(reverse('stickynotes'))
         self.assertEqual(response.status_code,200)
         self.assertContains(response, 'Test StickyNote')
 
-    def test_stickynote_get_returns_stickynote(self): 
+    def test_stickynote_get_returns_stickynote(self):
+        # Test that 'get' displays stickynote.
         stickynote = StickyNote.objects.get(id=1)
-        response = self.client.get(reverse('stickynote', args=[stickynote.id]))
+        response = self.client.get(reverse('stickynote',
+                                           args=[stickynote.id]))
         self.assertEqual(response.status_code,200)
         self.assertContains(response, 'Test StickyNote')
 
-    def test_delete_should_remove_stickynote(self): 
+    def test_delete_should_remove_stickynote(self):
+        # Test that 'delete' removes stickynote.
         stickynotes = StickyNote.objects.all()
         initial_length = stickynotes.count()
 
         stickynote = StickyNote.objects.get(id=1)
 
-        response = self.client.get(reverse('delete_stickynote', args=[stickynote.id]))
+        response = self.client.get(reverse('delete_stickynote',
+                                           args=[stickynote.id]))
 
         final_length = StickyNote.objects.all().count()
 
