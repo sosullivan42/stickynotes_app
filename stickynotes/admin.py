af602eaf4283c8f13cf118132eac295601ac8b37
Sorcha O'Sullivan <152802255+sosullivan42@users.noreply.github.com>
2024-07-05 23:10:33 +0100

    Update admin.py

--- stickynotes/admin.py
+++ stickynotes/admin.py
@@ -1,9 +1,9 @@
+# stickynotes/admin.py
+
 from django.contrib import admin
 from .models import StickyNote
 
 # Register your models here.
 
+# StickyNote model
 admin.site.register(StickyNote)
-
-
-
