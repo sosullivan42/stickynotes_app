802a3c03a1ed70b3c7c6621f1cfde93f460c0c09
Sorcha O'Sullivan <152802255+sosullivan42@users.noreply.github.com>
2024-07-05 23:11:12 +0100

    Update forms.py

--- stickynotes/forms.py
+++ stickynotes/forms.py
@@ -1,8 +1,21 @@
+# stickynote.forms.py
 from django import forms
 from .models import StickyNote
 
-class StickyNoteForm(forms.ModelForm): 
-    class Meta: 
+class StickyNoteForm(forms.ModelForm):
+    """
+    Form for creating and updating StickyNote objects.
+
+    Fields:
+    - title: CharField for the stickynote title.
+    - description: TextField for the stickynote description.
+
+    Meta class: 
+    - Defines the model to use (StickyNote) and the fields to include
+    in the form.
+
+    :parameter forms.ModelForm: Django's ModelForm class.
+    """
+    class Meta:
         model = StickyNote
         fields = ['title', 'description', 'mark_as_complete', 'created_by']
-        
